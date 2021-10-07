c7e8bdfdcefdf6eed093787114eab775186f47fe
Danny <handing@bu.edu>
2021-10-07 19:11:37 -0400

    try

--- visualization_function.R
+++ visualization_function.R
@@ -1,1 +1,7 @@
-“visualization_function.R”
+
+forest <- read.csv("forest_coverage_percent.csv")
+head(forest)
+
+income <- read.csv("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
+head(income)
+str(income)
---
+++ wrangling_code.R
@@ -0,0 +1,4 @@
+
+
+
+income <- read.csv("income_per_person_gdppercapita_ppp_inflation_adjus")
